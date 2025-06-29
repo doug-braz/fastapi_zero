bbae63d0c52912bc5c26f2b93550118dc93ce466
doug-braz <douglas.mateus29@gmail.com>
2025-06-29 18:39:26 -0300

    Aula 02 - Adicionando schema de mensagem

---
+++ fast_zero/schemas.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+# Ao definir o schema, garantimos o tipo de dados esperados, e isso também reflete na documentação da API
+class Message(BaseModel):
+    message: str
